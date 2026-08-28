0f628832c958e82939d58de7c625fdfae524f13e
Anshu Panwar <anshumona2005@gmail.com>
2026-08-29 01:19:14 +0530

    upgrade

--- scripts/worker.ts
+++ scripts/worker.ts
@@ -1,6 +1,17 @@
+import { createServer } from 'node:http'
+
 import { startWorkflowWorker } from '@/lib/workflow/worker'
 
 const worker = startWorkflowWorker()
+
+const port = Number(process.env.PORT) || 3000
+const server = createServer((_req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.end('ok')
+})
+server.listen(port, () => {
+  console.log(`[worker] health endpoint listening on port ${port}`)
+})
 
 worker.on('completed', (job) => {
   console.log(`[worker] job ${job.id} completed`)
@@ -15,6 +26,7 @@ console.log('[worker] workflow worker started')
 async function shutdown() {
   console.log('[worker] shutting down')
   await worker.close()
+  server.close()
   process.exit(0)
 }
 
